3d268be424626f33e2e32b46aee9cfed210a690f
Lou Vang <n11715791@qut.edu.au>
2024-10-10 10:39:24 +1000

    commit changes

--- website/auth.py
+++ website/auth.py
@@ -14,7 +14,7 @@ auth_bp = Blueprint('auth', __name__)
 def login():
     login_form = LoginForm()
     error = None
-    if login_form.validate_on_submit():
+    if (login_form.validate_on_submit()==True):
         user_name = login_form.user_name.data
         password = login_form.password.data
         user = db.session.scalar(db.select(User).where(User.name==user_name))
@@ -32,3 +32,38 @@ def login():
         else:
             flash(error)
     return render_template('user.html', form=login_form, heading='Login')
+
+
+@auth_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    register = RegisterForm()
+    #the validation of form is fine, HTTP request is POST
+    if (register.validate_on_submit()==True):
+            #get username, password and email from the form
+            uname = register.user_name.data
+            pwd = register.password.data
+            email = register.email_id.data
+            phone = register.phone_no.data
+            #check if a user exists
+            user = db.session.scalar(db.select(User).where(User.name==uname))
+            if user:#this returns true when user is not None
+                flash('Username already exists, please try another')
+                return redirect(url_for('auth.register'))
+            # don't store the password in plaintext!
+            pwd_hash = generate_password_hash(pwd)
+            #create a new User model object
+            new_user = User(name=uname, password_hash=pwd_hash, emailid=email, phonenum=phone)
+            db.session.add(new_user)
+            db.session.commit()
+            #commit to the database and redirect to HTML page
+            return redirect(url_for('main.index'))
+    #the else is called when the HTTP request calling this page is a GET
+    else:
+        return render_template('user.html', form=register, heading='Register')
+
+
+@auth_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
